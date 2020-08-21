6cc70e6694ef30e2f5c96ef50c5b440be541ea3f
Aashis Khanal <sraashis@gmail.com>
2020-08-21 13:26:17 -0400

    Batch Norm issue on ebval mode with low batch solved.

--- pooled.py
+++ pooled.py
@@ -8,9 +8,9 @@ import torch.nn.functional as F
 from torch.utils.data import ConcatDataset
 
 from core.measurements import Prf1a
-from models import MSANNet
 from core.utils import initialize_weights, NNDataLoader
 from local import FreeSurferDataset
+from models import MSANNet
 
 
 class PooledDataset(FreeSurferDataset):
@@ -21,17 +21,18 @@ class PooledDataset(FreeSurferDataset):
         self.mode = kw['mode']
 
 
-def get_dataset(conf, fold, split_key=None):
-    dataset = PooledDataset(data_dir=f"test/input/local{s}/simulatorRun/{conf['data_dir']['value']}",
-                            label_dir=f"test/input/local{s}/simulatorRun/{conf['label_dir']['value']}",
+def get_dataset(site, conf, fold, split_key=None):
+    dataset = PooledDataset(data_dir=f"test/input/local{site}/simulatorRun/{conf['data_dir']['value']}",
+                            label_dir=f"test/input/local{site}/simulatorRun/{conf['label_dir']['value']}",
                             mode=split_key)
-    split_file = f"test/input/local{s}/simulatorRun/{conf['split_dir']['value']}/SPLIT_{fold}.json"
+    split_file = f"test/input/local{site}/simulatorRun/{conf['split_dir']['value']}/SPLIT_{fold}.json"
     split = json.loads(open(split_file).read())
     dataset.load_indices(split[split_key])
     return dataset
 
 
 def eval(data_loader, model, device):
+    model.eval()
     score = Prf1a()
     for i, batch in enumerate(data_loader):
         inputs, labels = batch['inputs'].to(device).float(), batch['labels'].to(device).long()
@@ -43,15 +44,17 @@ def eval(data_loader, model, device):
     return score
 
 
-def train(fold, model, optim, device, epochs, train_loader, val_loader):
+def train(fold, model, optim, device, args, train_loader, val_loader):
     best_score = 0.0
-    for ep in range(epochs):
+    best_ep = 0
+    for ep in range(args['epochs']['value']):
         for i, batch in enumerate(train_loader):
             inputs, labels = batch['inputs'].to(device).float(), batch['labels'].to(device).long()
 
             optim.zero_grad()
             out = F.log_softmax(model(inputs), 1)
-            loss = F.nll_loss(out, labels)
+            wt = torch.randint(1, 101, (2,)).float()
+            loss = F.nll_loss(out, labels, weight=wt)
             loss.backward()
             optim.step()
 
@@ -59,54 +62,65 @@ def train(fold, model, optim, device, epochs, train_loader, val_loader):
             score = Prf1a()
             score.add(preds, labels)
             if i % int(math.log(i + 1) + 1) == 0:
-                print(f'Ep:{ep}/{epochs}, Itr:{i}/{len(train_loader)}, {round(loss.item(), 4)}, {score.prfa()}')
+                print(
+                    f"Ep:{ep}/{args['epochs']['value']}, Itr:{i}/{len(train_loader)}, {round(loss.item(), 4)}, {score.prfa()}")
         val_score = eval(val_loader, model, device)
         if val_score.f1 > best_score:
             best_score = val_score.f1
+            best_ep = ep
             torch.save(model.state_dict(), f'pooled_log/best_{fold}.pt')
             print(f'##### *** BEST saved ***  {best_score}')
         else:
             print('###### Not Improved:', val_score.f1, best_score)
 
+        if ep - best_ep >= args['patience']['value']:
+            break
+
 
 if __name__ == "__main__":
-    mode = 'train'
-    torch.backends.cudnn.enabled = False
-    hidden_sizes = []  # Todo
-    R = 8
-    LR = 0.001
-    BZ = 16
-    device = torch.device('cuda')
-    epochs = 5
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     os.makedirs('pooled_log', exist_ok=True)
-    global_score = Prf1a(0)
+    global_score = Prf1a()
+    inputspecs = json.loads(open('test/inputspec.json').read())
+    args = inputspecs[0]
+    args['epochs']['value'] = 101
+    args['batch_size']['value'] = 32
+    args['patience']['value'] = 31
+    # args['hidden_sizes']['value'] = [256, 128, 64, 32]
+
+    # inputspecs = [inputspecs[0]]
     for fold in range(10):
 
-        train_set, val_set, test_set = [], [], []
-        for s, conf in enumerate(json.loads(open('test/inputspec.json').read())):
-            train_set.append(get_dataset(conf, fold, 'train'))
-            val_set.append(get_dataset(conf, fold, 'validation'))
-            test_set.append(get_dataset(conf, fold, 'test'))
+        train_set, val_set = [], []
+        for s, conf in enumerate(inputspecs):
+            train_set.append(get_dataset(s, conf, fold, 'train'))
+            val_set.append(get_dataset(s, conf, fold, 'validation'))
 
-        model = nn.DataParallel(MSANNet(in_size=66, hidden_sizes=hidden_sizes, out_size=2))
+        model = nn.DataParallel(MSANNet(in_size=66, hidden_sizes=args['hidden_sizes']['value'], out_size=2))
         model = model.to(device)
+        torch.manual_seed(args['seed']['value'] if args.get('seed') else 122331)
         initialize_weights(model)
-        optim = torch.optim.Adam(model.parameters(), lr=LR)
+        optim = torch.optim.Adam(model.parameters(), lr=args['learning_rate']['value'])
 
-        run_test = mode == 'test'
-        if mode == 'train':
+        run_test = args['mode']['value'] == 'test'
+        if args['mode']['value'] == 'train':
             train_dset = ConcatDataset(train_set)
-            train_loader = NNDataLoader.new(dataset=train_dset, batch_size=BZ,
+            train_loader = NNDataLoader.new(dataset=train_dset, batch_size=args['batch_size']['value'],
                                             pin_memory=True, shuffle=True, drop_last=True)
             val_dset = ConcatDataset(val_set)
-            val_loader = NNDataLoader.new(dataset=val_dset, batch_size=BZ, pin_memory=True, shuffle=True)
+            val_loader = NNDataLoader.new(dataset=val_dset, batch_size=args['batch_size']['value'], pin_memory=True,
+                                          shuffle=True)
             print(f'Fold {fold}:', len(train_dset), len(val_dset))
-            train(fold, model, optim, device, epochs, train_loader, val_loader)
+            train(fold, model, optim, device, args, train_loader, val_loader)
             run_test = True
 
         if run_test:
+            test_set = []
+            for s, conf in enumerate(inputspecs):
+                test_set.append(get_dataset(s, conf, fold, 'test'))
             test_dset = ConcatDataset(test_set)
-            test_loader = NNDataLoader.new(dataset=test_dset, batch_size=BZ, pin_memory=True, shuffle=True)
+            test_loader = NNDataLoader.new(dataset=test_dset, batch_size=args['batch_size']['value'], pin_memory=True,
+                                           shuffle=True)
             model.load_state_dict(torch.load(f'pooled_log/best_{fold}.pt'))
 
             print(f'Fold {fold}:', len(test_dset))
