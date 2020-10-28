810f71932c8c913c54769d486b4c6df26ca8ce75
Sunitha Basodi <sunitha.basodi@gmail.com>
2020-10-28 01:43:07 -0400

    Adding python script which merges the profiler files

---
+++ profile_result_merger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+from coinstac_pyprofiler import custom_profile_merger as pm
+
+simulator_test_dir = './test'
+json_folder = 'profile_log_dir'
+pm.merge_json_computation(simulator_test_dir, 0, json_folder, "./test/profile_merge_results/", json_folder + "_test",
+                          save_format="json|html")
+
+# pm.merge_json_computation(simulator_test_dir, 5, "profiler_log/"+json_folder, "./test/profile_results/", json_folder, save_format="html")
