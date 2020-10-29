d8646895fd1576c5733def3001ff216f4da3cc49
Sunitha Basodi <sunitha.basodi@gmail.com>
2020-10-29 05:27:01 -0400

    Updated coinstac_pyprofiler version and corrected names

--- profile_result_merger.py
+++ profile_result_merger.py
@@ -3,7 +3,7 @@ from coinstac_pyprofiler import custom_profile_merger as pm
 
 simulator_test_dir = './test'
 json_folder = 'profile_log_dir'
-pm.merge_json_computation(simulator_test_dir, 0, json_folder, "./test/profile_merge_results/", json_folder + "_test",
+pm.merge_computation_json(simulator_test_dir, 0, json_folder, "./test/profile_merge_results/", json_folder + "_test",
                           save_format="json|html")
 
-# pm.merge_json_computation(simulator_test_dir, 5, "profiler_log/"+json_folder, "./test/profile_results/", json_folder, save_format="html")
+# pm.merge_computation_json(simulator_test_dir, 5, "profiler_log/"+json_folder, "./test/profile_results/", json_folder, save_format="html")
