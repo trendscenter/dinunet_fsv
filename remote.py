dd9aeca0c8a981327b888521aafa0ed8de6fe008
Aashis Khanal <sraashis@gmail.com>
2021-02-22 23:41:48 -0500

    Metrics set.

--- remote.py
+++ remote.py
@@ -3,6 +3,7 @@ import sys
 import json
 
 from coinstac_dinunet import COINNRemote
+from coinstac_dinunet.metrics import Prf1a
 #
 # import pydevd_pycharm
 #
@@ -15,6 +16,9 @@ class FSRemote(COINNRemote):
 
     def _set_log_headers(self):
         self.cache['log_header'] = 'loss,accuracy,f1'
+
+    def _new_metrics(self):
+        return Prf1a()
 
 
 if __name__ == "__main__":
