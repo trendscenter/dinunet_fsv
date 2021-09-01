7fb4c8cdfc961482e31fdeb94ea78783747f532f
Aashis Khanal <sraashis@gmail.com>
2021-08-31 23:39:37 -0400

    Running stats off.

--- entry.py
+++ entry.py
@@ -1,21 +1,26 @@
 from coinstac_dinunet import COINNLocal, COINNRemote
 from coinstac_dinunet.io import COINPyService
+
 from nn_implementations.fs import FreeSurferDataset, FreeSurferTrainer, FSVDataHandle
 from nn_implementations.vbm import VBMDataset, VBMTrainer, VBMDataHandle
 
 TASK_FS = "FS-Classification"
 TASK_VBM = "VBM-Classification"
 
+""" Test """
+task = TASK_FS
+agg_engine = 'dSGD'
+
 
 class Server(COINPyService):
     def get_local(self, msg) -> callable:
         pretrain_args = {'epochs': 11, 'batch_size': 16}
         dataloader_args = {"train": {"drop_last": True}}
-        local = COINNLocal(task_id=TASK_FS,
-                           cache=self.cache, input=msg['data']['input'], batch_size=2,
-                           state=msg['data']['state'], epochs=11, patience=21, model_scale=1,
+        local = COINNLocal(task_id=task,
+                           cache=self.cache, input=msg['data']['input'], batch_size=16,
+                           state=msg['data']['state'], epochs=31, patience=21, model_scale=1,
                            pretrain_args=None,
-                           dataloader_args=dataloader_args)
+                           dataloader_args=dataloader_args, agg_engine=agg_engine)
 
         if local.cache['task_id'] == TASK_FS:
             return local, FreeSurferTrainer, FreeSurferDataset, FSVDataHandle
