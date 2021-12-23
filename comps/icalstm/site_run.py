4a7b8c5872f6a077ab37d861d6fdefcb127b632a
Aashis Khanal <sraashis@gmail.com>
2021-12-23 11:25:41 -0500

    Remove gpus fom pretrain.

--- comps/icalstm/site_run.py
+++ comps/icalstm/site_run.py
@@ -3,6 +3,7 @@ from coinstac_dinunet.site_runner import SiteRunner
 from comps.icalstm import ICATrainer, ICADataHandle, ICADataset
 
 if __name__ == "__main__":
-    runner = SiteRunner(taks_id='FSL', data_path='../../datasets/icalstm', mode='Train',
-                        split_ratio=[0.8, 0.1, 0.1], monitor_metric='auc', log_header='Loss|Auc')
+    runner = SiteRunner(taks_id='ICA', data_path='../../datasets/icalstm', mode='Train',
+                        split_ratio=[0.6, 0.2, 0.2], monitor_metric='auc', log_header='Loss|Auc', batch_size=16,
+                        )
     runner.run(ICATrainer, ICADataset, ICADataHandle)
