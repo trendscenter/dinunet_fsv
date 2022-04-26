f49cf97c6dd35527ca68dfc059a024c869bdcd19
Aashis Khanal <sraashis@gmail.com>
2022-04-26 15:39:51 -0400

    LSTM cuda error fixed.

--- local.py
+++ local.py
@@ -9,7 +9,7 @@ from comps import NNComputation, FreeSurferDataset, FreeSurferTrainer, FSVDataHa
 from comps import ICATrainer, ICADataset, ICADataHandle
 
 """ Test """
-computation = NNComputation.TASK_FREE_SURFER
+computation = NNComputation.TASK_ICA
 agg_engine = AggEngine.DECENTRALIZED_SGD
 
 CACHE = {}
